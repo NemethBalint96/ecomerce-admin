14dfbb3fce3055852748da30cca55c9f3f170a7d
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-07-24 12:44:34 +0200

    setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Admin Dashboard</p>
+      <Button
+        size="sm"
+        variant="destructive"
+      >
+        Click Me
+      </Button>
+    </div>
+  )
+}
