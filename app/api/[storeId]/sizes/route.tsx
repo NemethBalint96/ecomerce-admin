d7267cbba3fc36c0ce035308af4d3ae99c649c0d
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-08-03 03:14:23 +0200

    sizes entity

---
+++ app/api/[storeId]/sizes/route.tsx
@@ -0,0 +1,56 @@
+import prismadb from "@/lib/prismadb"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request, { params }: { params: { storeId: string } }) {
+  try {
+    const { userId } = auth()
+    const body = await req.json()
+
+    if (!userId) {
+      return new NextResponse("Unathenticated", { status: 401 })
+    }
+
+    const { name, value } = body
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 })
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 })
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 })
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({ where: { id: params.storeId, userId: userId } })
+
+    if (!storeByUserId) {
+      return new NextResponse("Unathorized", { status: 403 })
+    }
+
+    const size = await prismadb.size.create({ data: { name, value, storeId: params.storeId } })
+
+    return NextResponse.json(size)
+  } catch (error) {
+    console.log("[SIZE_POST]", error)
+    return new NextResponse("Internal error", { status: 500 })
+  }
+}
+
+export async function GET(req: Request, { params }: { params: { storeId: string } }) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 })
+    }
+
+    const size = await prismadb.size.findMany({ where: { storeId: params.storeId } })
+
+    return NextResponse.json(size)
+  } catch (error) {
+    console.log("[SIZE_GET]", error)
+    return new NextResponse("Internal error", { status: 500 })
+  }
+}
